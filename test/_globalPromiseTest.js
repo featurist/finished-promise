0e4e746bc8ac0a7d85d2b75f2ee678fe464d38ce
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-04-28 12:40:41 +0100

    Add failing test to illustrate that async still uses native Promise :-/

--- test/_globalPromiseTest.js
+++ test/_globalPromiseTest.js
@@ -2,12 +2,17 @@ const assert = require('assert')
 const FinishedPromise = require('..')
 
 describe('Promise', () => {
+  const nativeThen = Promise.prototype.then
   before(() => {
+    Promise.prototype.then = function () {
+      throw new Error("native Promise still alive and kicking")
+    }
     FinishedPromise.install()
   })
 
   after(() => {
     FinishedPromise.uninstall()
+    Promise.prototype.then = nativeThen
   })
 
   it('is replaced with FinishedPromise', () => {
@@ -15,6 +20,16 @@ describe('Promise', () => {
     Promise.resolve(123).then(resolved => {
       result = resolved
     })
+    assert.equal(result, 123)
+  })
+
+  it('is replaced with FinishedPromise in async functions (failing)', () => {
+    let result
+
+    async function f(resolved) {
+      result = resolved
+    }
+    Promise.resolve(123).then(f)
     assert.equal(result, 123)
   })
 })
